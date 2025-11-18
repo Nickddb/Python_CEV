a0b85822be94ba292c15b7ea11c9c8dca1473ae6
Nickddb <nicollerocha.dev2024>
2025-11-18 15:05:41 -0300

    atualização

--- num primo.py
+++ num primo.py
@@ -1,7 +1,16 @@
 print("\033[7m NÚMERO PRIMO\033[m")
 num = int(input("Digite um número: "))
+tot = 0
+for p in range(1, num + 1):
+    if num % p == 0:
+        print("\033[32m", end='')
+        tot += 1
+    else:
+        print("\033[31m", end='')
+    print('{} '.format(p), end='')
+print("\n\033[mO número {} foi divisível {} vezes".format(num, tot))
 
-if num % 1 == 0 and num % num == 0:
-    print("Ele é primo!")
+if tot == 2:
+    print("Por isso ele é primo!")
 else:
-    print("Não é primo!")
+    print("Por isso ele NÃO é primo!")
--- palindromo.py
+++ palindromo.py
@@ -1,3 +1,11 @@
 print("PALÍNDROMO")
 
-frase = str(input("Digite uma frase: "))
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+print("Você digitou a frase \033[34m{}\033[m".format(palavras))
+if inverso == junto:
+    print("O palíndromo de sua frase é igual a: \033[35m", inverso, '\033[m')
+else:
+    print("E ela \033[31mnão\033[m é um palíndromo!")
