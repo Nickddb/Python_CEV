4671815687710db6f5083675e6412a9b44c11131
Nickddb <nicollerocha.dev2024>
2025-12-02 15:07:32 -0300

    maioridade

--- maioridade.py
+++ maioridade.py
@@ -1,17 +1,27 @@
 print("MAIORIDADE")
 from datetime import date
 atual = date.today().year
-soma = 0
-soma2 = 0
-
+maior = 0
+menor = 0
 
 for m in range(0,3):
     ano = int(input(f"Digite o ano de nascimento da {m+1}ª pessoa: "))
     idade = atual - ano
-for a in range(0,3):
-    if idade >= 18:
-        soma = soma + 1
-        print(soma, " pessoas atingiram a maioridade.")
+    if idade >= 21:
+        maior += 1
     else:
-        soma2 = soma2 + 1
-        print(soma2, " pessoas não atingiram a maioridade.")
+        menor += 1
+
+if maior == 1:
+    print("1 pessoa atingiu a maioridade.")
+if maior > 1:
+    print(maior, "pessoas atingiram a maioridade.")
+else:
+   print("Ninguém atingiu a maioridade.")
+
+if menor == 1:
+    print(" 1 pessoa não atingiu a maioridade.")
+if menor > 1:
+    print(menor, "pessoas não atingiram a maioridade.")
+else: 
+    print("Ninguém faltou atingir a maioridade")
