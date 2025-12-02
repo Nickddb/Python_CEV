a8b5207f96d66aecc07e66003875a356456b6f36
Nickddb <nicollerocha.dev2024>
2025-12-02 15:29:37 -0300

    peso

--- peso.py
+++ peso.py
@@ -1,5 +1,16 @@
 print("MAIOR PESO")
+maior = 0
+menor = 0
 
 for p in range(0,3):
     peso = float(input(f"Digite o peso da {p+1}ª pessoa: "))
-    if peso 
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print("O maior peso foi de: ", maior, " kg")
+print("O menor peso foi de: ", menor, " kg")
